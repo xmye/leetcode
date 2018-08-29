76b6a0a1baf972f95046960da9c41de27ab55e9f
xuemei-ye <0902130309@csu.edu.cn>
2018-08-29 20:29:33 +0800

    getTopK

---
+++ array/getTopK.py
@@ -0,0 +1,27 @@
+# def getTopK(arr,k):
+
+def quick_sort(arr,start,end,k):
+    div = partition(arr,start,end)
+    while k-1 != div:
+        if k-1 > div:
+            div = partition(arr,div+1,end)
+        if k-1 < div:
+            div = partition(arr,start,div)
+    if k-1 == div:
+        return arr[:k]
+
+
+def partition(arr,start,end):
+    i = start - 1
+    for j in range(start,end):
+        if arr[j] <= arr[end]:
+            i += 1
+            arr[i],arr[j] = arr[j],arr[i]
+    arr[i+1],arr[end] = arr[end],arr[i+1]
+    return i
+
+
+arr = [2,8,7,1,3,5,6,4]
+print("initial array:\n",arr)
+l = quick_sort(arr,0,len(arr)-1,2)
+print("result array:\n",l)
