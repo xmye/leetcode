1db994632063a29ec8e5bb81667813d8699d5f9f
xuemei-ye <0902130309@csu.edu.cn>
2018-08-30 21:35:19 +0800

    getMissingPositive

---
+++ array/getMissingPositive.py
@@ -0,0 +1,19 @@
+def getMissingPositive(arr):
+    n = len(arr)
+    i = 0
+    for i in range(len(arr)):
+        if arr[i] > 0 and arr[i] < n:
+            if (arr[i] - 1) != i and arr[arr[i]-1] != arr[i]:
+                tmp = arr[arr[i]-1]
+                arr[arr[i]-1] = arr[i]
+                arr[i] = tmp
+                i -= 1
+    print(arr)
+    for i in range(len(arr)):
+        if arr[i] - 1 != i:
+            return i+1
+    return n+1
+
+arr = [5,3,-1,2]
+print("initial array:\n",arr)
+print("result array:\n",getMissingPositive(arr))
